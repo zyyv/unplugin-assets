62aee9aa7b5e3df70059643e79aabfd06bd4afd2
Chris <hizyyv@gmail.com>
2024-01-06 19:43:09 +0800

    feat: dev command

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,16 +1,20 @@
-import type { Options } from 'tsup'
+import { defineConfig } from 'tsup'
 
-export default <Options>{
-  entryPoints: [
-    'src/*.ts',
-  ],
-  clean: true,
-  format: ['cjs', 'esm'],
-  dts: true,
-  onSuccess: 'pnpm run build:fix && pnpm run --filter ../client dev', 
-  shims: true,
-  treeshake: true,
-  define: {
-    'import.meta.DEV': JSON.stringify(!!process.env.DEV),
-  },
-}
+export default defineConfig(({ env }) => {
+  const isDev = env!.NODE_ENV === 'development'
+
+  return {
+    entryPoints: [
+      'src/*.ts',
+    ],
+    clean: true,
+    format: ['cjs', 'esm'],
+    dts: true,
+    onSuccess: `pnpm run build:fix ${isDev ? '&& pnpm run --filter ../client dev' : ''}`,
+    shims: true,
+    treeshake: true,
+    define: {
+      'import.meta.DEV': JSON.stringify(!!process.env.DEV),
+    },
+  }
+})
