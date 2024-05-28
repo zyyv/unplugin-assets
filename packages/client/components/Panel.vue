d3a73d0853ed58ac28face9ba3d2b550e35fd9c9
Chris <hizyyv@gmail.com>
2024-05-28 17:18:27 +0800

    fix(client): sticky position

--- packages/client/components/Panel.vue
+++ packages/client/components/Panel.vue
@@ -6,8 +6,6 @@ const props = defineProps<{
   list: AssetInfo[]
 }>()
 
-const [collapsed, toggleCollapsed] = useToggle(false)
-
 const images = computed(() => {
   return props.list.filter((item) => {
     return item.type === 'image'
@@ -27,24 +25,19 @@ function handlePreview(asset: AssetInfo) {
 </script>
 
 <template>
-  <div w-full b="~ dark:[hsla(0,0%,100%,.1)] #ccc" rd-lg of-hidden>
+  <section w-full b="~ dark:[hsla(0,0%,100%,.1)] #eee" rd-lg>
     <!-- Header -->
-    <div px-6 py-4 flex items-baseline bg="dark:#333 #ccc" pr>
+    <header px-6 py-4 flex items-baseline bg="dark:#333 #eee" ps top-16>
       <h2 text="lg dark:#eee #222">
         <i i-carbon-folder />
         {{ title }}
       </h2>
       <span ml-2 text-sm>{{ list.length }} items</span>
-      <div w-7 h-7 pa right-4 fcc cursor-pointer @click="toggleCollapsed()">
-        <i i-carbon-chevron-up trans important-duration-150 :class="collapsed ? 'rotate-180' : ''" />
-      </div>
-    </div>
+    </header>
     <!-- Content -->
-    <div :class="collapsed ? 'max-h-0' : 'max-h-120'" of-scroll trans important-duration-250>
-      <div v-if="list.length !== 0" px-6 py-4 flex="~ wrap" gap-6>
-        <AssetImage v-for="item in images" :key="item.path" :path="item.path" @click="handlePreview(item)" />
-        <AssetFontPreview v-for="item in fonts" :key="item.path" :asset="item" @click="handlePreview(item)" />
-      </div>
-    </div>
-  </div>
+    <main v-if="list.length !== 0" px-6 py-4 flex="~ wrap" gap-6>
+      <AssetImage v-for="item in images" :key="item.path" :path="item.path" @click="handlePreview(item)" />
+      <AssetFontPreview v-for="item in fonts" :key="item.path" :asset="item" @click="handlePreview(item)" />
+    </main>
+  </section>
 </template>
